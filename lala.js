4d06cb2d2805864aa0b3f5786f0a456c9aeb7c7b
Justina Kuchta <justyna.kuchta@gmail.com>
2019-08-11 19:46:11 +0000

    Automatically backed up by Learn

--- lala.js
+++ lala.js
@@ -6,3 +6,10 @@ const myHash = {a: "lala", b: 43, c: true}
 console.log(Object.keys(myHash))
 console.log(Object.entries(myHash))
 console.log(Object.getOwnPropertySymbols(myHash))
+
+var arr = ["under", "the", "hood"];
+
+arr[0];  // "under"
+arr['0']; // "under"
+arr[02]; // 02 the number *is* 2, so you get "hood"
+console.log(arr['2'])// '02' the string is *not* 2, so you get undefined
