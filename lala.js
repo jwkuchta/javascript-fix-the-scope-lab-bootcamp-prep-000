d693a79328977c0d95e3cce7a0bd0ff2da2de1b4
Justina Kuchta <justyna.kuchta@gmail.com>
2019-08-11 20:13:03 +0000

    Automatically backed up by Learn

--- lala.js
+++ lala.js
@@ -12,9 +12,9 @@ function fibonacci(num) {
 }
 
 
-console.log(fibonacci(1))
-console.log(fibonacci(2))
-console.log(fibonacci(3))
-console.log(fibonacci(4))
-console.log(fibonacci(5))
+//console.log(fibonacci(1))
+//console.log(fibonacci(2))
+//console.log(fibonacci(3))
+//console.log(fibonacci(4))
+//console.log(fibonacci(5))
 console.log(fibonacci(6))
