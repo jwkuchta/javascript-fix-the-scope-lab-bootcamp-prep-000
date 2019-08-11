59051b466fa81f1bb1de1eaef6651e0c23d9281a
Justina Kuchta <justyna.kuchta@gmail.com>
2019-08-11 20:02:32 +0000

    Automatically backed up by Learn

--- lala.js
+++ lala.js
@@ -1,19 +1,9 @@
-console.log(Object.keys([1, 2, 3, 4]))
-
-
-const myHash = {a: "lala", b: 43, c: true}
-
-console.log(Object.keys(myHash))
-console.log(Object.entries(myHash))
-console.log(Object.getOwnPropertySymbols(myHash))
-
-var arr = ["under", "the", "hood"];
-
-arr[0];  // "under"
-arr['0']; // "under"
-arr[02]; // 02 the number *is* 2, so you get "hood"
-console.log(arr['2'])// '02' the string is *not* 2, so you get undefined
-arr["string"] = "stringentry"
-var stringed = 3
-arr[stringed] = "stringedentry"
-console.log(arr)
+function fibonacci(num) {
+  for(let i = 0, i < num, i++) {
+    if(i<=1) {
+      return 1
+    } else {
+      return fibonacci(num-1) + fibonacci(num-2)
+    }
+  }
+}
