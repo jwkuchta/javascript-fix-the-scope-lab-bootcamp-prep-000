54606f4c71bc34626c45e6a33c0ab096c5cf45e2
Justina Kuchta <justyna.kuchta@gmail.com>
2019-08-11 20:09:24 +0000

    Automatically backed up by Learn

--- lala.js
+++ lala.js
@@ -1,13 +1,11 @@
 function fibonacci(num) {
   let result = 0
-  for(let i = 0; i < num; i ++) {
-    if(num <= 1) {
-      result = 1
-    } else {
-      result = [].push(fibonacci(num-1) + fibonacci(num-2))
-    }
+  if(num <= 1) {
+    result = 1
+  } else {
+    result = fibonacci(num-1) + fibonacci(num-2)
   }
-  return result
+  result
 }
 
 
