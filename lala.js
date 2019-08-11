6677c6aa91337a6368eeadb650480935bf11e80a
Justina Kuchta <justyna.kuchta@gmail.com>
2019-08-11 19:43:08 +0000

    Automatically backed up by Learn

---
+++ lala.js
@@ -0,0 +1,8 @@
+console.log(Object.keys([1, 2, 3, 4]))
+
+
+const myHash = {a: "lala", b: 43, c: true}
+
+console.log(Object.keys(myHash))
+console.log(Object.entries(myHash))
+console.log(Object.getOwnPropertySymbols(myHash))
